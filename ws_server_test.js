d13400133373b83cbed550a960ee0277c516d7e4
gmissan <gmissan@gmail.com>
2018-05-19 12:12:57 -0300

    Update ws_server_test.js

--- ws_server_test.js
+++ ws_server_test.js
@@ -1,13 +1,22 @@
 var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({port: 8080})
 
-wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-  })
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(data) {
+    // Broadcast to everyone else.
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+});
 
+  
+  
+  
   setInterval(
     () => ws.send(`${new Date()}`),
-    1000
+    3000
   )
 })
